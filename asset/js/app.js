a80c64fb8ebb5093fe3bb4764ca22f020628f571
unknown <bereketab.awa@gmail.com>
2019-05-11 00:23:24 +0200

    responsive menu and animated menu

---
+++ asset/js/app.js
@@ -0,0 +1,7 @@
+var menu = document.getElementById('button-menu');
+var nav = document.getElementById('nav');
+
+menu.addEventListener('click', function() {
+  // menu.classList.toggle("open");
+  nav.classList.toggle("open")
+})
