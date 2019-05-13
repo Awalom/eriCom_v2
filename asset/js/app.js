3004d5bc894919ec4a08fccdff0f0f058239e7d3
unknown <bereketab.awa@gmail.com>
2019-05-13 09:49:54 +0200

    before sorine

--- asset/js/app.js
+++ asset/js/app.js
@@ -5,3 +5,30 @@ menu.addEventListener('click', function() {
   // menu.classList.toggle("open");
   nav.classList.toggle("open")
 })
+
+
+// Swipper script
+
+var swiper = new Swiper('.swiper-container', {
+  effect: 'coverflow',
+  grabCursor: true,
+  centeredSlides: true,
+  slidesPerView: 'auto',
+  coverflowEffect: {
+    rotate: 0,
+    stretch: 1,
+    depth: 100,
+    modifier: 10,
+    slideShadows: false,
+  },
+  pagination: {
+    el: '.swiper-pagination',
+  },
+  keyboard: {
+    enabled: true,
+  },
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+});
